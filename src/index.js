559e0eb3bac7922dfa3db21370438bb7a3689c5e
DmiTsa <you@example.com>
2022-09-13 23:09:15 +0300

    13-09--13-09-2022

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
-// const text = 'React cool';
-// const elem = (
-//   <div>
-//     <h1>Hello world</h1>
-//     <h2>{text}</h2>
-//     <input type="text" />
-//     <button>1</button>
-//   </div>
-// );
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
