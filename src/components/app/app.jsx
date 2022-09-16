cf2d4c80a9f93271ca5f3f67ea2e837a35ecec05
Dmitry Taskaev <dmitrytaskaev.dt@gmail.com>
2022-09-16 16:39:45 +0300

    16-39--16-09-2022

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,26 +6,38 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 import '../search-panel/search-panel.css';
+import { Component } from 'react';
 
-function App() {
-  const data = [
-    { name: 'Вася Пупкин', salary: 500, increase: true, id: 1 },
-    { name: 'Семен Семеныч', salary: 400, increase: false, id: 2 },
-    { name: 'Игорь Валькович', salary: 1100, increase: false, id: 3 },
-    { name: 'Игорь Петрович', salary: 800, increase: true, id: 4 },
-  ];
-
-  return (
-    <div className="app">
-      <AppInfo />
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'Вася Пупкин', salary: 500, increase: true, id: 1 },
+        { name: 'Семен Семеныч', salary: 400, increase: false, id: 2 },
+        { name: 'Игорь Валькович', salary: 1100, increase: false, id: 3 },
+        { name: 'Игорь Петрович', salary: 800, increase: true, id: 4 },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <EmployeesList
+          data={this.state.data}
+          onDelete={(id) => {
+            console.log(id);
+          }}
+        />
+        <EmployeesAddForm />
       </div>
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/enployees-list/employees-list.jsx
+++ src/components/enployees-list/employees-list.jsx
@@ -1,9 +1,11 @@
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 
-function EmployeesList({ data }) {
+function EmployeesList({ data, onDelete }) {
   const cont = data.map((empl) => {
     const { id, ...emplMod } = empl;
-    return <EmployeesListItem key={id} {...emplMod} />;
+    return (
+      <EmployeesListItem key={id} {...emplMod} onDelete={() => onDelete(id)} />
+    );
   });
   return <ul className="app-list list-group">{cont}</ul>;
 }
